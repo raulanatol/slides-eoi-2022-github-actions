af56d957f672044e6f9b6f86abdd240699b852a0
raulanatol <raul@natol.es>
2022-09-18 15:21:37 +0100

    Update scripts

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -3,9 +3,11 @@
 set -eu
 
 rm -rf public
-mkdir -p public
+mkdir -p public/core/css public/core/config
 cp -R assets public/assets
-cp -R core public/core
+cp -R core/config public/core/config
+cp -R core/css public/core/css
+cp -R core/base/* public
 
 npm run -s og-image
 npm run -s deck
