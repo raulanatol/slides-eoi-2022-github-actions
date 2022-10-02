0c89fde6c79e7a44f3291a55a54ff732758658d7
raulanatol <raul@natol.es>
2022-10-02 17:09:35 +0100

    Update ribbon

--- core/components/ribbon.js
+++ core/components/ribbon.js
@@ -3,9 +3,9 @@ module.exports = {
   render: function (tokens, idx) {
     const m = tokens[idx].info.trim().match(/^ribbon\s+(.*)$/);
     if (tokens[idx].nesting === 1) {
-      return `<div class="ribbon-box"></div><div class="ribbon ribbon-${m[1]}">`;
+      return `<div class="ribbon ribbon-${m[1]}">`;
     } else {
-      return '</div></div>\n';
+      return '</div>\n';
     }
   }
 };
